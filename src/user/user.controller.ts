20cdbfe6970c16c304558c62ea2f986516f35a09
Luan Vendt <luan.vendt@bitize.com.br>
2023-08-22 16:56:40 -0300

    class user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UserController {
@@ -7,7 +8,7 @@ export class UserController {
     constructor(private userService: UserService) {}
 
     @Post()
-    public create(@Body() user) {
+    public create(@Body() user: User): User {
         const userCreated = this.userService.create(user)
 
         return userCreated
