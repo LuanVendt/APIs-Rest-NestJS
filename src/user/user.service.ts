f2da5c521cd214ba3637d90ab202c02d8d8eb128
Luan Vendt <luan.vendt@bitize.com.br>
2023-08-22 16:32:57 -0300

    Initial commit

---
+++ src/user/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common"
+
+@Injectable()
+export class UserService {
+    private users = []
+
+    public create(user){
+        this.users.push(user)
+
+        return user
+    }
+}
